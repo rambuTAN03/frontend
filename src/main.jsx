60294d66a1684a9dce449e186f07dacbb6d75105
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2023-10-25 11:50:31 +0800

    remove saga theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import './index.css' 
-import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import 'primeicons/primeicons.css'; 
 
 import {
   createBrowserRouter,
