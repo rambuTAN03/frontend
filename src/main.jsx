7e10b5328eea74eb1950ae420544729bea2b090a
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2023-10-25 11:52:01 +0800

    commented prime-react css

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import './index.css' 
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'; 
+//import 'primereact/resources/themes/saga-blue/theme.css';
+//import 'primereact/resources/primereact.min.css';
+//import 'primeicons/primeicons.css'; 
 
 import {
   createBrowserRouter,
